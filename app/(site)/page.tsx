52a7461b0a71a79c600d4b118ac44ba426d5efe5
Aman <jaman207168@gmail.com>
2023-08-10 16:37:12 +0530

    setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,39 @@
+export default function Home() {
+    return (
+      <div
+        className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+        "
+      >
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+                alt="logo"
+                height="48"
+                width="48"
+                className="mx-auto w-auto"
+                src="/images/logo.webp"
+                />
+            <h2
+                className="
+                mt-6
+                text-center
+                text-3xl
+                font-bold
+                tracking-tight
+                text-gray-900
+                "
+            >
+                Sign in to your Account
+            </h2>
+        </div>
+        {/* AuthForm */}
+      </div>
+    )
+}
