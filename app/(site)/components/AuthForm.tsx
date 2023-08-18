e2a36ce831703196a09c59b44d9cfc87d33f9164
Aman <jaman207168@gmail.com>
2023-08-18 22:48:35 +0530

    db-setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/input';
+
 import { useCallback , useState } from 'react';
 import { FieldValues , SubmitHandler, useForm } from 'react-hook-form';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import AuthSocialButton from './AuthSocialButton';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -73,13 +77,91 @@ const Authform = () => {
                     className='space-y-6'
                     onSubmit={handleSubmit(onSubmit)}
                 >
+                    {variant == 'REGISTER' && (
+                        <Input
+                            id='name'
+                            label="Name"
+                            register={register}
+                            errors={errors}
+                            disabled={isLoading}  
+                        />   
+                    )}
                     <Input
                         id='email'
-                        label="Email"
+                        label="Email Address"
+                        type='email'
                         register={register}
-                        errors={errors}    
+                        errors={errors}
+                        disabled={isLoading}    
                     />
+                    <Input
+                        id='password'
+                        label="Password"
+                        type='password'
+                        register={register}
+                        errors={errors}
+                        disabled={isLoading}    
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant == 'LOGIN' ? 'Sign in' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+
+                <div className='mt-6'>
+                    <div className='relative'>
+                        <div className='
+                            absolute
+                            inset-0
+                            flex
+                            intems-center
+                            '
+                        >
+                            <div className='w-full border-t border-gray-300'/>
+                        </div>
+                        <div className='relative flex justify-center text-sm' >
+                            <span className='bg-white px-2 text-gray-500'>
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className='mt-6 flex gap-2'>
+                        <AuthSocialButton 
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton 
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                    
+                    <div className='
+                        flex
+                        gap-2
+                        justify-center
+                        text-sm
+                        mt-6
+                        px-2
+                        text-gray-500
+                    '>
+                        <div>
+                            {variant === 'LOGIN' ? 'New to Messager-Plus?' : 'Already have an account?'}
+                        </div>
+                        <div 
+                            onClick={toggleVariant}
+                            className='underline cursor-pointer'
+                        >
+                            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
